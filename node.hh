6ff3288427c23c119117e2a0bc843e362a7986b1
Alex Hoy <aghoy03@gmail.com>
2023-12-03 14:40:16 -0500

    syntax fix

--- node.hh
+++ node.hh
@@ -1,5 +1,6 @@
 #ifndef NODE_HH
 #define NODE_HH
+#include <stddef.h>
 
 using namespace std;
 
@@ -8,28 +9,28 @@ class Node
 {
 public:
   // Constructors -- All assume Node will be Red. All Pointers will be assigned to NUlL unless specified
-  Node(){}
-  Node(*Node parent, *Node leftChild, *Node rightChild, T value = NULL) { this.rightChild = rightchild; this.leftChild = leftChild; this.parent = parent; this.value = value; }
-  Node(*Node parent, T value = NULL) { this.parent = parent; this.value = value; }
+  Node(){};
+  Node(Node *parent, Node *leftChild, Node *rightChild, T value = NULL) { rightChild = rightChild; leftChild = leftChild; parent = parent; value = value; }
+  Node(Node *parent, T value = NULL) { parent = parent; value = value; }
 
   // Returns corresponding pointer to node
-  *Node leftChild() { return leftChild; } 
-  *Node rightChild() { return rightChild; }
-  *Node parent() { return parent; }
+  Node *getLeftChild() { return leftChild; } ;
+  Node *getRightChild() { return rightChild; };
+  Node *getParent() { return parent; };
 
   // Set corresponding pointers
-  void setLeftChild(*Node child) { leftChild = child; }
-  void setRightChild(*Node child) { rightChild = child; }
-  void setParent(*Node node) { parent = node; }
+  void setLeftChild(Node *child) { leftChild = child; };
+  void setRightChild(Node *child) { rightChild = child; };
+  void setParent(Node *node) { parent = node; };
 
-  void setColor(bool is_red) { red = is_red; }
+  void setColor(bool is_red) { red = is_red; };
   
 private:
-  boolean red = true;
+  bool red = true;
   T value = NULL;
-  *Node parent = NULL;
-  *Node leftChild = NULL;
-  *Node rightChild = NULL;
+  Node *parent = NULL;
+  Node *leftChild = NULL;
+  Node *rightChild = NULL;
 };
 
 
